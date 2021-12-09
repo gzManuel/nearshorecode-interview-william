a6d1beeed0ca9c5d94868799270d53a2808bc8c5
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-12-09 13:39:47 -0400

    feat: add api setBook

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
-
+import BookModel from '../models/Book'
 const backendDomain = "http://localhost:3333";
+
+
 
 export const getBooks = async () => {
   try {
@@ -14,6 +16,16 @@ export const getBooks = async () => {
 export const getBook = async (slug: string) => {
   try {
     const response = await axios.get(`${backendDomain}/books/${slug}`);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const setBook = async (book: BookModel) => {
+  try {
+    const response = await axios.post(`${backendDomain}/books`,book.toJson());
+    console.log(response);
     return response.data.data;
   } catch (error) {
     console.log(error);
