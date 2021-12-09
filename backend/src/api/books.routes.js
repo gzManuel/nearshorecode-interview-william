f9a3d0f32d6a2e131959a0c21bef9f55e996fb7f
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-12-09 10:34:48 -0400

    feat: Add POST /books

--- backend/src/api/books.routes.js
+++ backend/src/api/books.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getBook, getBooks } from '../services/books'
+import { getBook, getBooks, postBook, } from '../services/books'
 
 const router = express.Router();
 
 router.get('/books', getBooks);
 
 router.get('/books/:slug', getBook);
+router.post('/books', postBook);
+
 
 export default router;
--- backend/src/services/books.js
+++ backend/src/services/books.js
@@ -1,14 +1,28 @@
 import database from "../database"
 
+const db = database();
 
 export const getBooks = async (req, res) => {
-    const db = database();
     const books = await db.getBooks();
     res.status(200).json({ data: books });
 }
 
-export const getBook = async (req, res)=>{
-    const db = database();
+export const getBook = async (req, res) => {
     const book = await db.getBook(req.params.slug);
-    return res.status(200).json({data:book});
+    return res.status(200).json({ data: book });
+}
+
+export const postBook = async (req, res) => {
+    const body = req.body;
+    // const book = {
+    //     title: body.,
+    //     slug: 'cracking-the-code-interview',
+    //     isbn: '193-454-344',
+    //     author: 'Gayle Laakmann McDowell',
+    //     publisher: 'Career up',
+    //     synopsis: `Cracking the Coding Interview, 6th Edition is here to help you through this process, teaching you what you need to know and enabling you to perform at your very best. I've coached and interviewed hundreds of software engineers. The result is this book.`
+    // }
+    
+    const bookSaved = await db.setBook('cracking-the-code-interview', body);
+    return res.status(200).json({ data: bookSaved })
 }
