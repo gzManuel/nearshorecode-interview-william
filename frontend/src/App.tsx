2c56d8dda5d75d37a186add5c295be66325dab0a
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-12-09 11:29:01 -0400

    refactor: Add typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Routes>
+        <Route path="/"  />
+      </Routes>
     </div>
   );
 }
