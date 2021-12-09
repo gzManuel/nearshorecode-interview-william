b278119e29c3e2dc537b779abf2660a942348b53
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-12-09 10:24:15 -0400

    feat: Add GET /books/:slug

--- backend/src/api/books.routes.js
+++ backend/src/api/books.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getBooks } from '../services/books'
+import { getBook, getBooks } from '../services/books'
 
 const router = express.Router();
 
 router.get('/books', getBooks);
+
+router.get('/books/:slug', getBook);
 
 export default router;
--- backend/src/services/books.js
+++ backend/src/services/books.js
@@ -4,5 +4,11 @@ import database from "../database"
 export const getBooks = async (req, res) => {
     const db = database();
     const books = await db.getBooks();
-    res.status(200).json({ data: books })
+    res.status(200).json({ data: books });
+}
+
+export const getBook = async (req, res)=>{
+    const db = database();
+    const book = await db.getBook(req.params.slug);
+    return res.status(200).json({data:book});
 }
