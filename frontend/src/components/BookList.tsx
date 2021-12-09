a232c69dbd95acb37ab879772f38171dde47c524
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-12-09 17:28:42 -0400

    feat: Moves to the clicked book

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -1,27 +1,31 @@
 import { List } from "antd";
 import { useEffect, useState } from "react";
 import { getBooks } from "../api";
-import BookInterface from '../models/Book'
+import BookInterface from "../models/Book";
+import { useNavigate } from "react-router-dom";
 
 const BookList: React.FC = (props) => {
   const [books, setBooks] = useState<BookInterface[]>([]);
+  const navigate = useNavigate();
 
   const setData = async () => {
     const booksRetrieved = await getBooks();
     setBooks(booksRetrieved);
   };
 
+  const moveToBook = (slug:string) => {
+    navigate(`/books/${slug}`)
+  };
+
   useEffect(() => {
     setData();
   }, []);
 
-  const listBooks = books.map((book) => 
-
-    <List.Item key={book.slug}>
+  const listBooks = books.map((book) => (
+    <List.Item key={book.slug} onClick={()=>moveToBook(book.slug)}>
       <List.Item.Meta title={book.title} description={book.synopsis} />
     </List.Item>
-
-  );
+  ));
 
   return <List>{listBooks}</List>;
 };
